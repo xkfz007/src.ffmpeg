6cea0661c436aaa4b41c19aab8df1ee6cdfa83af
Fangzhen <xkfz007@users.noreply.github.com>
2015-05-24 08:38:48 +0800

    add tutorial02

--- ffmpeg_learning/ffmpeg_learning/main_test.cpp
+++ ffmpeg_learning/ffmpeg_learning/main_test.cpp
@@ -1,5 +1,7 @@
 extern int main_tutorial01(int argc, char *argv[]);
+extern int main_tutorial02(int argc, char *argv[]);
 
 int main(int argc,char* argv[]){
-    main_tutorial01(argc,argv);
+    //main_tutorial01(argc,argv);
+    main_tutorial02(argc,argv);
 }

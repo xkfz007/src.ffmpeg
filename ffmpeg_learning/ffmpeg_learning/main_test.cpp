7af99a40997634aad17c56bf703b7767defa30cd
Fangzhen <xkfz007@users.noreply.github.com>
2015-05-25 15:07:04 +0800

    some changes

--- ffmpeg_learning/ffmpeg_learning/main_test.cpp
+++ ffmpeg_learning/ffmpeg_learning/main_test.cpp
@@ -5,10 +5,6 @@ extern int main_tutorial04(int argc, char *argv[]);
 extern int main_tutorial05(int argc, char *argv[]);
 extern int main_tutorial06(int argc, char *argv[]);
 extern int main_tutorial07(int argc, char *argv[]);
-extern int main_tutorial08(int argc, char *argv[]);
 int main(int argc,char* argv[]){
-    //main_tutorial01(argc,argv);
-    //main_tutorial02(argc,argv);
-    main_tutorial03(argc,argv);
-    //main_tutorial04(argc,argv);
+    main_tutorial07(argc,argv);
 }

8df0af74bd4a2c33e85e64b5181cb7af68007821
Fangzhen <xkfz007@users.noreply.github.com>
2015-07-26 15:27:39 +0800

    add ff_codec project compiled well

---
+++ ff_codec/h264_dec.c
@@ -0,0 +1,17 @@
+
+//#include "h264.h"
+#include <stdio.h>
+int main()
+{
+    //AVCodecContext tmp;
+    //AVCodecContext* avctx=&tmp;
+    //avctx->priv_data=
+    //
+    //if(ff_h264_decode_init(avctx)) {
+    //    fprintf(stdout,"could not creat decoder\n");
+    //    return -1;
+    //}
+
+
+    return 0;
+}
---
+++ ff_codec/h264_dec.h
@@ -0,0 +1,9 @@
+#ifndef _H264_DEC_H_
+#define _H264_dEC_H_
+//typedef struct AVCodecContext {
+//    void *priv_data;
+//    int workaround_bugs;
+//    int flags;
+//
+//}AVCodecContext;
+#endif

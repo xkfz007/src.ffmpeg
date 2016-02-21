328a3fe02b8aec63a724f29c30e42dbfa146c254
Fangzhen <xkfz007@users.noreply.github.com>
2016-02-21 11:36:20 +0800

    well compiled

--- ffmpeg-2.7-simple/cmdutils.c
+++ ffmpeg-2.7-simple/cmdutils.c
@@ -1081,10 +1081,18 @@ static void print_all_libs_info(int flags, int level)
     PRINT_LIB_INFO(avformat, AVFORMAT, flags, level);
     PRINT_LIB_INFO(avdevice, AVDEVICE, flags, level);
     PRINT_LIB_INFO(avfilter, AVFILTER, flags, level);
+#if CONFIG_AVRESAMPLE
     PRINT_LIB_INFO(avresample, AVRESAMPLE, flags, level);
+#endif
+#if CONFIG_SWSCALE
     PRINT_LIB_INFO(swscale,  SWSCALE,  flags, level);
+#endif
+#if CONFIG_SWRESAMPLE
     PRINT_LIB_INFO(swresample,SWRESAMPLE,  flags, level);
+#endif
+#if CONFIG_POSTPROC
     PRINT_LIB_INFO(postproc, POSTPROC, flags, level);
+#endif
 }
 
 static void print_program_info(int flags, int level)

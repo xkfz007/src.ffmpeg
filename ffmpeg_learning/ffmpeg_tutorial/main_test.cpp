6c5a273c26920104eaef7b648a6c029a5c3a0e82
Fangzhen <xkfz007@users.noreply.github.com>
2016-08-28 18:54:53 +0800

    add ffmpeg player tutorial

---
+++ ffmpeg_learning/ffmpeg_tutorial/main_test.cpp
@@ -0,0 +1,10 @@
+extern int main_tutorial01(int argc, char *argv[]);
+extern int main_tutorial02(int argc, char *argv[]);
+extern int main_tutorial03(int argc, char *argv[]);
+extern int main_tutorial04(int argc, char *argv[]);
+extern int main_tutorial05(int argc, char *argv[]);
+extern int main_tutorial06(int argc, char *argv[]);
+extern int main_tutorial07(int argc, char *argv[]);
+int main(int argc,char* argv[]){
+    main_tutorial04(argc,argv);
+}
